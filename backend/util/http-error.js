8af55cb8cc694bc29ec4b876f0346b0a58296f2e
rayan200 <2232174@cmontmorency.qc.ca>
2024-09-20 21:37:53 -0400

    recommencer

--- backend/util/http-error.js
+++
@@ -1,8 +0,0 @@
-class HttpError extends Error {
-  constructor(message, statusCode) {
-    super(message); // Ajoute un message d'erreur
-    this.statusCode = statusCode; // Définit le code de statut HTTP de l'erreur
-  }
-}
-
-module.exports = HttpError;
